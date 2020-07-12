de83de61bdd2c3ea1f4bef9b4d51825680dac920
Smekalin Sergei <crypto.mnr@gmail.com>
2020-07-12 16:23:10 +0300

    Finished HW
    1. Сделал бакдроп+модальное окно
    2. Закончил разметку форм
    3. Заменил чекбокс на кастомный

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,13 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  document.querySelector('.clients-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    console.log('');
+    console.count('Submit #');
+
+    new FormData(e.currentTarget).forEach((value, name) => console.log(name, ':', value));
+  });
+})();
